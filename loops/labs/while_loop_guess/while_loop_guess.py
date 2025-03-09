279638ef2f7925e4279432f26b06fdae0084cf6d
corydave <corydave@users.noreply.github.com>
2025-03-09 00:13:14 +0000

    more support files

--- loops/labs/while_loop_guess/while_loop_guess.py
+++ loops/labs/while_loop_guess/while_loop_guess.py
@@ -1,80 +1,21 @@
 # FILE NAME - while_loop_guess.py
 
-# NAME: 
-# DATE: 
-# BRIEF DESCRIPTION:  
+# NAME - 
+# DATE - 
+# DESCRIPTION - 
 
+def main():
+    while_loop_guess()
 
+def while_loop_guess():
 
-# 1. Make sure you fill out the comments above
-# 2. Write your code in the proper spot
-# 3. Be sure to answer the Reflection Questions and Attestation below
-# 4. The Sample Output has been included in this code for your convenience
+    guess = -1;
+    num_guesses = 0
 
+    while (guess != 33):
+        guess = int(input('Guess a number: '))
+        num_guesses = num_guesses + 1
 
+    print('You guessed in', num_guesses, 'tries.')
 
-
-
-
-########## ENTER YER CODE BELOW THIS LINE ##########
-
-
-
-
-
-
-
-
-
-########### END YER CODE ABOVE THIS LINE ###########
-
-
-
-
-########################################
-#          SAMPLE OUTPUT
-########################################
-
-'''
-Guess a number: 22
-Guess a number: 44
-Guess a number: 33
-You guessed in 3 tries.
-'''
-
-
-'''
-Guess a number: 33
-You guessed in 1 tries.
-'''
-
-
-'''
-Guess a number: -9
-Guess a number: 100
-Guess a number: 30
-Guess a number: 32
-Guess a number: 34
-Guess a number: 33
-You guessed in 6 tries.
-'''
-
-
-
-########################################
-#            ATTESTATION
-########################################
-
-'''
-
-It is critical in this class that you understand the concepts as we explore them because
-those concepts are required understanding for entry level programming. Reliance on resources
-like AI and internet sites like Chegg, CourseHero, StackOverflow, and general Google results
-may impede your understanding. Please rate how well you understand the concepts in this lab: 
-
-[ ] I understand very little about this lab.
-[ ] I am about 50/50 on this lab; I get parts of it but not the whole picture.
-[ ] I pretty much get it.
-[ ] I'm solid. Totally got it.
-
-'''
+main()
