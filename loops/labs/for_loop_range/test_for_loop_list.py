22ba0a9115391b8733984f2595b1efc46a684ac7
corydave <corydave@users.noreply.github.com>
2025-03-10 11:40:42 +0000

    tests for labs

---
+++ loops/labs/for_loop_range/test_for_loop_list.py
@@ -0,0 +1,16 @@
+class TestInput:
+        
+    def test_input_from_user(self):
+
+        with open("actual.txt") as actual_output:
+            actual = ''
+            for line in actual_output:
+                actual += line.strip() + '\n'
+
+        with open("expected.txt") as expected_output:
+            expected = ''
+            for line in expected_output:
+                expected += line.strip() + '\n'
+
+        assert actual.strip() == expected.strip()            
+
